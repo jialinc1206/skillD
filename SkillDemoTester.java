0020a7fc9e24cf42fd495b6bdc86e804497de207
Aaron Chen <aaronchen@MacBook.lan>
2022-02-04 16:56:43 -0800

    demo+test

---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void Double() {
+        assertEquals(3, SkillDemo.Double(1));
+    }
+}
